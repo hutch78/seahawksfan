48492c95966928b73b093555e7490b1eaa794eb6
Jeremy Hutchcraft <hutch78@me.com>
2015-01-31 17:35:59 -0500

    added stream replies

--- seahawksfan.js
+++ seahawksfan.js
@@ -13,6 +13,8 @@ var firstRunThrough = true;
 
 var ownerID = 3007901165;
 
+var prev_targets = [];
+
 console.log("SeahawksFan Activated!!!.\n\n");
 
 
@@ -31,6 +33,89 @@ var post_tweet = function(tweet_text, waiting){
 }
 
 
+/* =- =- =- -= =- =- =- =- =- =- =- =- =- =- =- =- =- =- =- =- =- =-
+
+	Listen for tweets containing our hashtag, and reply to them
+
+=- =- =- =- =- =- =- =- -==- =- =- =- -= =- =- =- =- =-= - =- =- =- -=*/
+var stream = T.stream('statuses/filter', { track: '#patriotsnation', language: 'en' })
+console.log
+stream.on('tweet', function (tweet) {
+
+	var random_int = Math.floor((Math.random() * 100) + 1);
+
+	if(random_int > 90){	// 10% of the time
+		// spam filter
+		has_link = tweet.text.indexOf("http"),
+		has_mention = tweet.text.indexOf("@"),
+		has_rt = tweet.text.indexOf("RT");
+
+		var has_run = function(){
+			prev_targets.forEach(function(tweet){
+				if(prev_targets[tweet] == victim_id){
+					return true;
+				}
+			});
+		}
+
+		if(has_link == -1 && has_rt == -1 && has_mention == -1){
+				var victim = tweet,
+					victim_name = victim.user.screen_name,
+					// victim_name = "Patriots",
+					victim_id = victim.id,
+					tweet_bodies = [
+					"@"+victim_name+" #LegionofBoom is coming to AZ to destroy you! #patriots!",
+					"@"+victim_name+" GOOOO @SEAHAWKS! :) http://bit.ly/1wjWxxP ",
+					"@"+victim_name+" Cheaters!! xD GO @Seahawks #deflategate ",
+					"@"+victim_name+"We're shutting down #tombrady on Sunday! #LegionOfBoom @"+victim_name,
+					"@"+victim_name+" - #TomBrady is going DOWN!!! #LEGIONOFBOOM @Seahawks #Seahawks #patriotsnation",
+					"@Seattle is coming for you @"+victim_name+"... http://bit.ly/15Vx5nH #LegionOfBoom",
+					"I'm seeing a lot of patriots fans here talking trash! @"+victim_name+" & #patriotsnation, we are coming for you! #LegionOfBoom"
+				];
+
+			var trash_text = tweet_bodies[Math.floor((Math.random() * (tweet_bodies.length - 1)) + 1)];
+
+			/* =- =- =- -= =- =- =- =- =- =- =- =- =- =- =- =- =- =-
+
+				Post The Tweet
+
+			=- =- =- =- =- =- =- =- -==- =- =- =- -= =- =- =- =-*/
+			if (has_run != true) {
+
+				console.log('\nhas_run = false');
+				console.log('caught a live one!\n\n');
+
+				// =- =- =- =- =- =- =- =- =-
+				//
+				//		Fire Away!
+				//
+				// =- =- =- =- =- =- =- =-
+				post_tweet(trash_text,true);
+
+				console.log(trash_text);
+
+				prev_targets.push(victim_id);
+
+
+				// Write messsages to the console
+				console.log("tweet id = "+victim_id);
+				console.log("victim = "+victim_name+"\n\n");
+
+
+			} else {
+
+				console.log("We already targeted this user. No Post Made.");
+
+			}
+
+		}
+	} else {
+		firstRunThrough = false;
+	}
+
+	
+
+});
 
 /* =- =- =- -= =- =- =- =- =- =- =- =- =- =- =- =- =- =-
 
@@ -50,8 +135,7 @@ var talk_trash = function(the_query, num_tweets){
 	T.get("search/tweets", { q: the_query, count: num_tweets }, function(err, data, response) {
 
 		var 	tweets = data.statuses,
-				legit_tweets = [],
-				prev_targets = [];
+				legit_tweets = [];
 
 		/* =- =- =- -= =- =- =- =- =- =- =- =- =- =- =- =- =- =-
 
@@ -89,22 +173,28 @@ var talk_trash = function(the_query, num_tweets){
 		=- =- =- =- =- =- =- =- -==- =- =- =- -= =- =- =- =-*/
 		var rand_integer = Math.floor(Math.random() * legit_tweets.length),
 			victim = legit_tweets[rand_integer],
-			// victim_name = victim.user.screen_name,
-			victim_name = "Patriots",
+			victim_name = victim.user.screen_name,
+			// victim_name = "Patriots",
 			victim_id = victim.id,
 			tweet_bodies = [
 				"@"+victim_name+" #LegionofBoom is coming to AZ to destroy you! #patriots!",
 				"@"+victim_name+" GOOOO @SEAHAWKS! :) http://bit.ly/1wjWxxP ",
 				"@"+victim_name+" Cheaters!! xD GO @Seahawks #deflategate ",
-				"@"+victim_name+"We're shutting down #tombrady on Sunday! #LegionOfBoom @"+victim_name,
+				"@"+victim_name+"We're shutting down #tombrady on Sunday! #LegionOfBoom",
 				"12th man visits SUPER BOWL XLIX #LEGIONOFBOOM!",
-				"@"+victim_name+" - #TomBrady is going DOWN!!! #LEGIONOFBOOM @Seahawks #Seahawks #patriotnation",
+				"@"+victim_name+" - #TomBrady is going DOWN!!! #LEGIONOFBOOM @Seahawks #Seahawks #patriotsnation",
 				"@Seattle is coming for you @"+victim_name+"... http://bit.ly/15Vx5nH #LegionOfBoom",
 				"I'm seeing a lot of patriots fans here talking trash! @"+victim_name+" & #patriotsnation, we are coming for you! #LegionOfBoom"
 			];
 
 		var trash_text = tweet_bodies[Math.floor((Math.random() * (tweet_bodies.length - 1)) + 1)];
 
+
+		/* =- =- =- -= =- =- =- =- =- =- =- =- =- =- =- =- =- =-
+
+			Check if we have replied to this user already
+
+		=- =- =- =- =- =- =- =- -==- =- =- =- -= =- =- =- =-*/
 		var has_run = function(){
 			prev_targets.forEach(function(tweet){
 				if(prev_targets[tweet] == victim_id){
@@ -120,7 +210,7 @@ var talk_trash = function(the_query, num_tweets){
 		=- =- =- =- =- =- =- =- -==- =- =- =- -= =- =- =- =-*/
 		if (has_run != true) {
 
-			console.log('has_run = false');
+			console.log('New target acquired, interval post');
 
 			// =- =- =- =- =- =- =- =- =-
 			//
@@ -176,10 +266,13 @@ setInterval(function(){
 		talk_trash("#patriotsnation since:2015-1-11", 50);
 	} else {
 		firstRunThrough = false;
-		console.log('do_run == false, skipping this round\n\n');
+		console.log('Interval Post - do_run == false, skipping this round\n\n');
 	}
 
 // one hour twenty three minutes
 }, 383000);
+
+// 40 seconsd
+// }, 40000);
 
 
